64b93411f35fa389ae5919e8759b33ad8267a047
Ari Russo <ari.russo@gmail.com>
2013-01-19 19:53:51 -0500

    only load the project once

--- test/helper.rb
+++ test/helper.rb
@@ -5,3 +5,15 @@ $LOAD_PATH.unshift dir + "/../lib"
 
 require "test/unit"
 require "live-large"
+
+class TestHelper
+
+  class << self
+    attr_reader :project
+  end
+
+  def self.project
+    @project ||= LiveLarge::Project.new("/Users/russo/Documents/ableton/10.2 Project/10.2.als")
+  end
+
+end
