d095186964bd8e614aa578c91876953a49d7abfa
Ari Russo <ari.russo@gmail.com>
2014-04-05 16:30:32 -0400

    use mocha/shoulda

--- test/files_test.rb
+++ test/files_test.rb
@@ -1,18 +1,21 @@
-#!/usr/bin/env ruby
-
 require 'helper'
 
 class LiveProject::FilesTest < Test::Unit::TestCase
 
-  def project_files
-    LiveProject::Files.new("test/assets/test Project/test.als", "test/scratch")
-  end
+  context "Files" do
 
-  def test_copy_to_workspace
-    @project_files = project_files
-    @project_files.send(:copy_to_workspace)
-    assert File.exists?(project_files.scratch[:path])
+    context "#copy_to_workspace" do
+
+      setup do
+        @project_files = LiveProject::Files.new("test/assets/test Project/test.als", "test/scratch")
+      end
+
+      should "copy to workspace" do
+        @project_files.send(:copy_to_workspace)
+        assert File.exists?(@project_files.scratch[:path])
+      end
+
+    end
   end
 
 end
-
--- test/project_test.rb
+++ test/project_test.rb
@@ -1,61 +1,72 @@
-#!/usr/bin/env ruby
-
 require 'helper'
 
 class LiveProject::ProjectTest < Test::Unit::TestCase
 
-  def test_populate_data
-    @project = TestHelper.project
-    assert_not_nil @project.data
-    assert_equal Hash, @project.data.class
-  end
+  context "Project" do
 
-  def test_populate_tracks
-    @project = TestHelper.project
-    assert_not_nil @project.tracks
-    assert_not_empty @project.tracks
-    assert_not_nil @project.tracks[:midi]
-    assert_not_empty @project.tracks[:midi]
-    assert @project.tracks[:midi].first.kind_of?(LiveProject::Track::MIDI)
-  end
+    setup do
+      @project = TestHelper.project
+    end
 
-  def test_getter
-    @project = TestHelper.project
-    assert_equal @project.data["Ableton"]["Creator"], @project["Creator"]
-  end
+    context ".project" do
 
-  def test_setter
-    @project = TestHelper.project
-    @project["Creator"] = "blah"
-    assert_equal "blah", @project["Creator"]
-    assert_equal "blah", @project.data["Ableton"]["Creator"]
-  end
+      should "populate project" do
+        assert_not_nil @project.data
+        assert_equal Hash, @project.data.class
+      end
 
-  def test_getter_alias
-    @project = TestHelper.project
-    assert_equal @project.data["Ableton"]["Creator"], @project[:created_with_version]
-  end
+      should "populate tracks" do
+        @project = TestHelper.project
+        assert_not_nil @project.tracks
+        assert_not_empty @project.tracks
+        assert_not_nil @project.tracks[:midi]
+        assert_not_empty @project.tracks[:midi]
+        assert @project.tracks[:midi].first.kind_of?(LiveProject::Track::MIDI)
+      end
 
-  def test_setter_alias
-    @project = TestHelper.project
-    @project[:created_with_version] = "blah!!"
-    assert_equal "blah!!", @project[:created_with_version]
-    assert_equal "blah!!", @project.data["Ableton"]["Creator"]
-  end
+      should "have working getters" do
+        assert_equal @project.data["Ableton"]["Creator"], @project["Creator"]
+      end
 
-  def test_save
-    @project = TestHelper.project
-    original_file_size = File.size(@project.files.origin[:path])
-    @project.save
-    assert_not_equal original_file_size, File.size(@project.files.scratch[:path])
-  end
+      should "have working setters" do
+        @project["Creator"] = "blah"
+        assert_equal "blah", @project["Creator"]
+        assert_equal "blah", @project.data["Ableton"]["Creator"]
+      end
 
-  def test_unzip
-    @project = TestHelper.project
-    FileUtils.rm(@project.files.xml[:path])
-    @project.send(:unzip)
-    assert File.exists?(@project.files.xml[:path])
-    assert File.size(@project.files.xml[:path]) > 0
+      should "have working getter alias" do
+        assert_equal @project.data["Ableton"]["Creator"], @project[:created_with_version]
+      end
+
+      should "have working setter alias" do
+        @project[:created_with_version] = "blah!!"
+        assert_equal "blah!!", @project[:created_with_version]
+        assert_equal "blah!!", @project.data["Ableton"]["Creator"]
+      end
+
+    end
+
+    context "#save" do
+
+      should "save file" do
+        original_file_size = File.size(@project.files.origin[:path])
+        @project.save
+        assert_not_equal original_file_size, File.size(@project.files.scratch[:path])
+      end
+
+    end
+
+    context "#unzip" do
+
+      should "unzip the raw project file" do
+        FileUtils.rm(@project.files.xml[:path])
+        @project.send(:unzip)
+        assert File.exists?(@project.files.xml[:path])
+        assert File.size(@project.files.xml[:path]) > 0
+      end
+
+    end
+
   end
 
 end
